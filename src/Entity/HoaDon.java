adfd0d88533ab73aba77d65e80123a57a64146fd
unknown <tongnguyennhattien140805@gmail.com>
2025-10-31 11:21:22 +0700

    update HoaDon

--- src/Entity/HoaDon.java
+++ src/Entity/HoaDon.java
@@ -6,17 +6,18 @@ import java.util.List;
 public class HoaDon {
 	private String maHoaDon;
 	private LocalDateTime ngayLap;
-	private KhachHang khachHang;
 	private List<ChiTietHoaDon> chiTiet;
 	private double maGiamGia;
 	private boolean trangThaiThanhToan;
 
-	public HoaDon(String maHoaDon, LocalDateTime ngayLap, KhachHang khachHang, List<ChiTietHoaDon> chiTiet,
+	public HoaDon() {
+	}
+
+	public HoaDon(String maHoaDon, LocalDateTime ngayLap,  List<ChiTietHoaDon> chiTiet,
 			double maGiamGia, boolean trangThaiThanhToan) {
 		super();
 		this.maHoaDon = maHoaDon;
 		this.ngayLap = ngayLap;
-		this.khachHang = khachHang;
 		this.chiTiet = chiTiet;
 		this.maGiamGia = maGiamGia;
 		this.trangThaiThanhToan = trangThaiThanhToan;
@@ -36,14 +37,6 @@ public class HoaDon {
 
 	public void setNgayLap(LocalDateTime ngayLap) {
 		this.ngayLap = ngayLap;
-	}
-
-	public KhachHang getKhachHang() {
-		return khachHang;
-	}
-
-	public void setKhachHang(KhachHang khachHang) {
-		this.khachHang = khachHang;
 	}
 
 	public List<ChiTietHoaDon> getChiTiet() {
@@ -69,21 +62,21 @@ public class HoaDon {
 	public void setTrangThaiThanhToan(boolean trangThaiThanhToan) {
 		this.trangThaiThanhToan = trangThaiThanhToan;
 	}
-	
+
 	public double tinhTongTien() {
-	    double tong = 0;
+		double tong = 0;
 
-	    if (chiTiet != null) {
-	        for (ChiTietHoaDon cthd : chiTiet) {
-	            tong += cthd.tinhThanhTien();
-	        }
-	    }
+		if (chiTiet != null) {
+			for (ChiTietHoaDon cthd : chiTiet) {
+				tong += cthd.tinhThanhTien();
+			}
+		}
 
-	    if (maGiamGia > 0 && maGiamGia <= 100) {
-	        tong -= tong * (maGiamGia / 100);
-	    }
+		if (maGiamGia > 0 && maGiamGia <= 100) {
+			tong -= tong * (maGiamGia / 100);
+		}
 
-	    return tong;
+		return tong;
 	}
 
 }
